46e38d4f43ab6ce42c56b757d711df10c255136c
Rodion3000 <rodion.papizh@mail.ru>
2025-09-19 14:59:35 +0500

    1

--- Assets/Project/Dev/GamePlay/AnimatorLogic/AnimatorState.cs
+++ Assets/Project/Dev/GamePlay/AnimatorLogic/AnimatorState.cs
@@ -4,7 +4,6 @@ namespace Project.Dev.GamePlay.AnimatorLogic
     public enum AnimatorState
     {
         Unknow = -1,
-        Idle = 0,
         Walk = 1,
         StrafeLeft = 2,
         TurnLeft = 3,
--- Assets/Project/Dev/GamePlay/NPC/Player1/HeroAnimator.cs
+++ Assets/Project/Dev/GamePlay/NPC/Player1/HeroAnimator.cs
@@ -8,17 +8,15 @@ namespace Project.Dev.GamePlay.NPC.Player1
     public class HeroAnimator : MonoBehaviour, IAnimatorStateReader
     {
         // animator params
-        private static readonly int IdleHash = Animator.StringToHash("Idle");
         private static readonly int StrafeLeftHash = Animator.StringToHash("StrafeLeft");
-        private static readonly int TurnLeftHash = Animator.StringToHash("TurnLeft5");
+        private static readonly int TurnLeftHash = Animator.StringToHash("TurnLeft");
         private static readonly int TurnRightHash = Animator.StringToHash("TurnRight");
         private static readonly int WalkHash = Animator.StringToHash("Walk");
 
         //animator states
-        private readonly int _idleStateHash = Animator.StringToHash("Idle");
         private readonly int _strafeLeftHash = Animator.StringToHash("StrafeLeft");
         private readonly int _walkingStateHash = Animator.StringToHash("Walk");
-        private readonly int _turnLeftHash = Animator.StringToHash("TurnLeft5");
+        private readonly int _turnLeftHash = Animator.StringToHash("TurnLeft");
         private readonly int _turnRightHash = Animator.StringToHash("TurnRight");
 
         [SerializeField] private Animator animator;
@@ -28,10 +26,6 @@ namespace Project.Dev.GamePlay.NPC.Player1
         public AnimatorState State { get; private set; }
 
         #region Play methods
-
-        [HorizontalGroup("Actions")] [Button("Idle", ButtonStyle.FoldoutButton), GUIColor(0.9f, 0.9f, 0.9f)]
-        public void PlayIdle(bool idleState) =>
-            animator.SetBool(IdleHash, idleState);
 
         [HorizontalGroup("Actions")]
         [Button ("StrafeLeft"), GUIColor(0,0,1)]
@@ -70,10 +64,9 @@ namespace Project.Dev.GamePlay.NPC.Player1
         {
             AnimatorState state;
 
-            if      (stateHash == _idleStateHash)    state = AnimatorState.Idle;
-            else if (stateHash == _walkingStateHash)  state = AnimatorState.Walk;
-            else if (stateHash == _turnRightHash)     state = AnimatorState.TurnRight;
-            else if (stateHash == _turnLeftHash)   state = AnimatorState.TurnLeft;
+            if (stateHash == _walkingStateHash)      state = AnimatorState.Walk;
+            else if (stateHash == _turnRightHash)    state = AnimatorState.TurnRight;
+            else if (stateHash == _turnLeftHash)     state = AnimatorState.TurnLeft;
             else if (stateHash == _strafeLeftHash)   state = AnimatorState.StrafeLeft;
             else                                     state = AnimatorState.Unknow;
 
