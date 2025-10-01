b786c4ec0e9f708ff478a3ea5af9769a9b59a30a
Rodion3000 <rodion.papizh@mail.ru>
2025-10-01 14:28:53 +0500

    1

--- Assets/Project/Dev/GamePlay/NPC/Player1/HeroMove.cs
+++ Assets/Project/Dev/GamePlay/NPC/Player1/HeroMove.cs
@@ -28,59 +28,67 @@ namespace Project.Dev.GamePlay.NPC.Player1
             Move();
         }
 
-        private void Move()
+         private void Move()
+    {
+        var movementVector = Vector3.zero;
+
+        if (_inputService.MoveAxis.sqrMagnitude > 0.001f)
         {
-            var movementVector = Vector3.zero;
+            var input = _inputService.MoveAxis;
 
-            if (_inputService.MoveAxis.sqrMagnitude > 0.001f)
+            // Ограничение движения по основной оси (по вашему оригиналу)
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
             {
-                var input = _inputService.MoveAxis;
-
-                if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
-                {
-                    input.y = 0;
-                }
-                else
-                {
-                    input.x = 0;
-                }
-
-                movementVector = _camera.transform.TransformDirection(input);
-                movementVector.y = 0;
-                movementVector.Normalize();
-            }
-
-            movementVector += Physics.gravity;
-
-            characterController.Move(movementVector * (movementSpeed * Time.deltaTime));
-
-            Vector3 Speed = characterController.velocity;
-            Speed.y = 0;
-            if (Speed.sqrMagnitude > 0.001f)
-            {
-                Vector3 localMove = transform.InverseTransformDirection(Speed);
-                float horizontalMove = localMove.x;
-                float verticalMove = localMove.z;
-
-                if (Mathf.Abs(horizontalMove) > Mathf.Abs(verticalMove))
-                {
-                    if (horizontalMove > 0)
-                        heroAnimator.PlayStrafeRight(characterController.velocity.magnitude);
-                    else
-                        heroAnimator.PlayStrafeLeft(characterController.velocity.magnitude);
-                }
-                else
-                {
-                    if (verticalMove > 0)
-                        heroAnimator.PlayWalk(characterController.velocity.magnitude);
-                }
+                input.y = 0;
             }
             else
             {
-                heroAnimator.PlayWalk(0);
-                heroAnimator.PlayStrafeLeft(0);
-                heroAnimator.PlayStrafeRight(0);
+                input.x = 0;
+            }
+
+            // Получаем Y-угол героя
+            float heroYAngle = transform.eulerAngles.y;
+            Quaternion heroYRotation = Quaternion.Euler(0, heroYAngle, 0);
+
+            // Преобразовываем входные данные относительно героя
+            movementVector = heroYRotation * new Vector3(input.x, 0, input.y);
+
+            movementVector.y = 0;
+            movementVector.Normalize();
+        }
+
+        movementVector += Physics.gravity;
+
+        characterController.Move(movementVector * (movementSpeed * Time.deltaTime));
+
+        // Анализ скорости для анимаций
+        Vector3 Speed = characterController.velocity;
+        Speed.y = 0;
+        if (Speed.sqrMagnitude > 0.001f)
+        {
+            // Текущая локальная скорость относительно героя
+            Vector3 localMove = transform.InverseTransformDirection(Speed);
+            float horizontalMove = localMove.x;
+            float verticalMove = localMove.z;
+
+            if (Mathf.Abs(horizontalMove) > Mathf.Abs(verticalMove))
+            {
+                if (horizontalMove > 0)
+                    heroAnimator.PlayStrafeRight(characterController.velocity.magnitude);
+                else
+                    heroAnimator.PlayStrafeLeft(characterController.velocity.magnitude);
+            }
+            else
+            {
+                    heroAnimator.PlayWalk(characterController.velocity.magnitude);
             }
         }
+        else
+        {
+            heroAnimator.PlayWalk(0);
+            heroAnimator.PlayStrafeLeft(0);
+            heroAnimator.PlayStrafeRight(0);
+        }
+    }
     }
 }
